bec87401e4cc329d29c5000180660fae0e141a12
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-28 15:49:05 -0300

    test change git remote set-url origin

--- TP3Grupo18/Inicio.aspx.designer.cs
+++ TP3Grupo18/Inicio.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace TP3Grupo18
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control btnVolverClick.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnVolverClick;
     }
 }
